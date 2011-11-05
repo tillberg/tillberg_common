02bfa3f6e1b7bae7bc6d023e144167094e4bf363
Dan Tillberg <dan@tillberg.us>
2011-11-05 14:33:31 -0400

    autocommit

--- globals.js
+++ globals.js
@@ -1,4 +1,6 @@
 
+var color = exports.color = require("./ansi-color");
+
 global.proto = require('./proto').proto;
 global.exec = require('./exec').exec;
 global.Timer = require('./exec').Timer;
@@ -31,30 +33,29 @@ global.debug = function(x, name) {
   console.error((name ? name + ': ' : '') + util.inspect(x));
 };
 
-var color = require("./ansi-color").set;
 var events  = require('events');
 var logger = new events.EventEmitter();
 exports.logger = logger;
 
 global.error = function(s) {
   logger.emit('data', 'error', s);
-  console.error(color(s, "red"));
+  console.error(color.set(s, "red"));
 };
 
 global.warn = function(s) {
   logger.emit('data', 'warn', s);
-  console.log(color(s, "yellow"));
+  console.log(color.set(s, "yellow"));
 };
 
 
 global.info = function(s) {
   logger.emit('data', 'info', s);
-  console.log(color(s, "black", true));
+  console.log(color.set(s, "black", true));
 };
 
 global.verbose = function(s) {
   logger.emit('data', 'verbose', s);
-  //console.log(color(s, "black", true));
+  //console.log(color.set(s, "black", true));
 };
 
 global.throttled = throttled;
